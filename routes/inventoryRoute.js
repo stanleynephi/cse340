22f7b06fb28ae9987085b53934f5f181f93e3632
stanleynephi <nul23001@byui.edu>
2025-02-03 10:07:04 +0000

    update on small screen render

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,14 +4,15 @@ create a new instance for the express router
 and a new instance for the inventory controller.*/
 const express = require('express');
 const router = express.Router()
+const Util = require("../utilities/")
 
 //inventory controller
 const invntoryController = require("../Contoller/inventoryController")
 
 
 //build the route to build the inventory by classification view
-router.get('/type/:classificationId', invntoryController.buildInventoryByClassification)
-router.get("/detail/:inv_Id",invntoryController.buildInventoryByCarDetails)
+router.get('/type/:classificationId', Util.handleErrors(invntoryController.buildInventoryByClassification))
+router.get("/detail/:inv_Id",Util.handleErrors(invntoryController.buildInventoryByCarDetails))
 
 //export the router instance
 module.exports = router
